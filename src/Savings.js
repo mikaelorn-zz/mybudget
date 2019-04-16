c269ccdc1be020eebe813f5237ddc9f833f6ff89
mikaelorn <fabianmikaelorn@gmail.com>
2019-04-16 18:06:47 +0200

    savings

--- src/Savings.js
+++ src/Savings.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
-import NumberFormat from 'react-number-format';
-import { Container, Row, Col } from 'react-grid-system';
-import { setConfiguration } from 'react-grid-system';
+// import NumberFormat from 'react-number-format';
+// import { Container, Row, Col } from 'react-grid-system';
+// import { setConfiguration } from 'react-grid-system';
+import * as Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+    rangeSelector: {
+        selected: 1
+    },
+    title: {
+        text: "Totalen"
+    },
+    series: [
+        {
+            name: "Sum",
+            tooltip: {
+                valueDecimals: 2
+            },
+            data: [
+                [
+                    1492358498000,
+                    0
+                ],
+                [
+                    1555430498000,
+                    101682
+                ]
+            ]
+        }
+    ]
+}
 
 class Savings extends Component {
 
@@ -10,6 +39,11 @@ render() {
     return(
         <div>
             <p1>Spara</p1>
+            <HighchartsReact
+                highcharts={Highcharts}
+                constructorType={'stockChart'}
+                options={options}
+            />
         </div>
     );
     }
